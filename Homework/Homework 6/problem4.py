12951711e294b3797b6fd1726968dd8ec4cebc74
Jaden Snell <jasn7628@colorado.edu>
2024-04-22 20:01:15 -0600

    finished code

--- Homework/Homework 6/problem4.py
+++ Homework/Homework 6/problem4.py
@@ -1,11 +1,20 @@
 import numpy as np
 from scipy.special import gamma
+from scipy.integrate import quad
+from numpy.polynomial.laguerre import laggauss
 
 def composite_trapezoidal_rule(a, b, f, N,k):
     delta_x = (b - a) / (N - 1)
     x_values = [a + i*delta_x for i in range(N)]
-    sum_f_x = sum(f(x,k) for x in x_values[1:-1])
-    return delta_x * ((f(a,k) + f(b,k)) / 2 + sum_f_x)
+    function_evaluations = 0
+    sum_f_x = 0
+    for x in x_values:
+        sum_f_x += f(x, k)
+        function_evaluations += 1
+    result = delta_x * ((f(a, k) + f(b, k)) / 2 + sum_f_x )
+    function_evaluations += 2  
+    return result, function_evaluations
+    
 
 def test_function(x,k):
     return x**(k-1) * np.exp(-x)
@@ -20,7 +29,34 @@ def truncation_point(k, tol = 1e-6):
 for k in [2, 4, 6, 8, 10,20]:
     a, b = 0, truncation_point(k) 
     N = int(b)*100  # Number of intervals
-    numeric_integral = composite_trapezoidal_rule(a, b, test_function, N,k)
+    numeric_integral,neval = composite_trapezoidal_rule(a, b, test_function, N,k)
     gamma_value = gamma(k)
-    print(f"Gamma({k}) = {gamma_value}, Numeric = {round(numeric_integral,6)}, Relative Error = {abs(gamma_value - numeric_integral)/gamma_value}")
+    print(f"Gamma({k}) = {gamma_value}, Numeric = {round(numeric_integral,6)}, Relative Error = {abs(gamma_value - numeric_integral)/gamma_value}, Function Evaluations = {neval}")
     print("Truncation point:", round(b,2))
+    
+#Python quad
+print("\nPython Quadrature")
+for k in [2, 4, 6, 8, 10, 20]:
+    a, b = 0, truncation_point(k)
+    integral_result = quad(test_function, a, b, args=(k),full_output=1)
+    gamma_value = gamma(k)
+    numeric_integral = integral_result[0]
+    neval = integral_result[2]['neval']
+    print(f"Gamma({k}) = {gamma_value}, Numeric Quadrature Routine = {round(numeric_integral, 6)}, Relative Error = {abs(gamma_value - numeric_integral) / gamma_value}, Function Evaluations = {neval}")
+    print("Truncation point:", round(b, 2))
+
+
+def gamma_approx(t, n):
+    # Obtain the n points and weights for Gauss-Laguerre quadrature
+    x, w = laggauss(n)
+    # Calculate the approximate integral for Gamma(t)
+    integral_approx = np.sum(w * x**(t-1))
+    return integral_approx
+
+t_values = [2, 3, 4, 5, 6]  # Example values for t
+
+
+for k in [2, 4, 6, 8, 10, 20]:
+    approx = gamma_approx(k, 10)
+    exact = gamma(k)
+    print(f"Gamma({k}) = {exact}, Approximation = {approx}, Relative Error = {abs(exact - approx)/exact}")
